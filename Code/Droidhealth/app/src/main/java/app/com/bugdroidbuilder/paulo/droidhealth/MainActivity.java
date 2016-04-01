44a0325ba792a5ea1a0b672104eb768dd62ae51b
pauloXtr3m <paulo-gyn@outlook.com>
2016-04-01 18:01:46 -0300

    Codificação iniciada

---
+++ Code/Droidhealth/app/src/main/java/app/com/bugdroidbuilder/paulo/droidhealth/MainActivity.java
@@ -0,0 +1,13 @@
+package app.com.bugdroidbuilder.paulo.droidhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
